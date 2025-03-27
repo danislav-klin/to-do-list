b74902f8a547d0bf362ad2b958d6c72cd9ef97ea
danil <dkalimullin470@gmail.com>
2025-03-27 15:04:47 +0500

    add html form login and register

--- resources/views/login.blade.php
+++ resources/views/login.blade.php
@@ -1,5 +1,19 @@
 @extends('layouts.app')
 
 @section('content')
+
+<form class="w-50 mx-auto">
+    <div class="mb-3">
+      <label for="email" class="form-label">login</label>
+      <input name="email" type="email" class="form-control" id="email">
+      
+    </div>
+    <div class="mb-3">
+      <label for="password" class="form-label">Password</label>
+      <input name="password" type="password" class="form-control" id="password">
+    </div>
+    
+    <button type="submit" class="btn btn-primary">Submit</button>
+</form>
     
 @endsection
--- resources/views/register.blade.php
+++ resources/views/register.blade.php
@@ -1,5 +1,28 @@
 @extends('layouts.app')
 
 @section('content')
+<form class="w-50 mx-auto">
+    <div class="mb-3">
+        <label for="email" class="form-label">Email</label>
+        <input name="email" type="email" class="form-control" id="email">
+        
+      </div>
+    <div class="mb-3">
+        <label for="name" class="form-label">name</label>
+        <input name="name" type="name" class="form-control" id="name">
+        
+      </div>
+    <div class="mb-3">
+      <label for="email" class="form-label">login</label>
+      <input name="email" type="email" class="form-control" id="email">
+      
+    </div>
+    <div class="mb-3">
+      <label for="password" class="form-label">Password</label>
+      <input name="password" type="password" class="form-control" id="password">
+    </div>
     
+    
+    <button type="submit" class="btn btn-primary">Submit</button>
+</form>
 @endsection
